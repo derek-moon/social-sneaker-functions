54f880fa4c1408d4c2c1fe751aae4f17bdc21870
mxxnus <lxnkzr@gmail.com>
2019-12-02 20:08:30 -0500

    FINALLY added user signup (I had days of npm / firebase packaging issues)

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,34 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://socialsneakers.firebaseio.com"
-});
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZmA264sG7O9vsiRFTwk1fYaiVSLle9yc",
+  authDomain: "socialsneakers.firebaseapp.com",
+  databaseURL: "https://socialsneakers.firebaseio.com",
+  projectId: "socialsneakers",
+  storageBucket: "socialsneakers.appspot.com",
+  messagingSenderId: "911853218668",
+  appId: "1:911853218668:web:a152b23d037cc8ae14a149",
+  measurementId: "G-7YFPZ3KJ9C"
+};
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://socialsneakers.firebaseio.com"
+// });
 
 const express = require('express');
 const app = express();
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
 
 
 app.get('/screams', (req,res)=>{
@@ -54,6 +75,30 @@ app.post('/scream',(req, res) => {
     });
 });
 
+
+//signujp route
+
+app.post('/signup', (req,res) =>{
+  const newUser = {
+    email:req.body.email,
+    password:req.body.password,
+    confirmPassword:req.body.confirmPassword,
+    handle:req.body.handle,
+  };
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return res.status(201).json({message: `user ${data.user.uid} signed up successfully`})
+    })
+    .catch(err =>{
+      console.error(err);
+      return res.status(500).json({error:err.code})
+    })
+})
+
+
+
+
 //setting up api\
 //https://us-central1-socialsneakers.cloudfunctions.net/api
 exports.api = functions.https.onRequest(app);
+ 
