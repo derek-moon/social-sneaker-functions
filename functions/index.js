205045f4c94e0c51e223d52245a3bf9d3a114e10
mxxnus <lxnkzr@gmail.com>
2019-12-03 15:30:08 -0500

    field validations are working..

--- functions/index.js
+++ functions/index.js
@@ -81,9 +81,20 @@ app.post('/scream',(req, res) => {
     });
 });
 
+const isEmpty = (string) => {
+  if(string.trim() === '') return true;
+  else return false;
+}
 
-//signujp route
+//helper 
+const isEmail = (email) =>{
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(email.match(regEx)) return true;
+  else return false;
+}
 
+
+//signup route
 app.post('/signup', (req,res) =>{
   const newUser = {
     email:req.body.email,
@@ -92,6 +103,23 @@ app.post('/signup', (req,res) =>{
     handle:req.body.handle,
   };
 
+  let errors = {};
+
+  if (isEmpty(newUser.email)){
+    errors.email = 'Must not be empty'
+  } else if(!isEmail(newUser.email)){
+    errors.email = 'Must be a valid email address'
+  }
+
+  //hepler password
+
+if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
+if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+if(Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+  //todo validate data 
   let token, userId;
   db.doc(`/users/${newUser.handle}`).get()
     .then(doc =>{
