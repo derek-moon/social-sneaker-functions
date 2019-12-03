4e97b010b5e5a4ac8f24c367f96fb54d8163c084
mxxnus <lxnkzr@gmail.com>
2019-12-02 21:11:58 -0500

    added user data to database and authen in firebase

--- functions/index.js
+++ functions/index.js
@@ -92,6 +92,7 @@ app.post('/signup', (req,res) =>{
     handle:req.body.handle,
   };
 
+  let token, userId;
   db.doc(`/users/${newUser.handle}`).get()
     .then(doc =>{
       if(doc.exists){
@@ -102,15 +103,32 @@ app.post('/signup', (req,res) =>{
     })
     //authen token
     .then(data => {
+      userId = data.user.uid;
       return data.user.getIdToken();
       
     })
-    .then(token =>{
+    .then(idToken =>{
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      //into collection
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials)
+    })
+    .then(()=>{
       return res.status(201).json({token});
     })
     .catch(err=>{
       console.error(err);
-      return res.status(500).json({error:err.code})
+      if(err.code === 'auth/email-already-in-use'){
+        return res.status(400).json({email: 'Email is already in use'})
+      }else {
+        return res.status(500).json({error:err.code});
+      }
+      
     })
 
 
