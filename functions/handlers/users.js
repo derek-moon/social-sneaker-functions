c827c4fb0aa1e95d2b454ef1231198bd8cc11640
mxxnus <lxnkzr@gmail.com>
2019-12-11 14:40:08 -0500

    added notifications to USER GET req

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -124,6 +124,22 @@ exports.signup = (req,res) =>{
         data.forEach(doc =>{
           userData.likes.push(doc.data());
         })
+        return db.collection('notifications').where('recipient', '==', req.user.handle)
+          .orderBy('createdAt','desc').limit(10).get();
+      })
+      .then(data => {
+        userData.notifications = [];
+        data.forEach(doc => {
+          userData.notifications.push({
+            recipient: doc.data().recipient,
+            sender: doc.data().sender,
+            createdAt: doc.data().createdAt,
+            screamId: doc.data().screamId,
+            type: doc.data().type,
+            read: doc.data().recipient,
+            notificationId: doc.id
+          });
+        });
         return res.json(userData);
       })
       .catch(err =>{
