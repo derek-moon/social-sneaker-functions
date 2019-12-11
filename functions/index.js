9be98082fee0e0e3e20dafa0871f1c0858ead0ef
mxxnus <lxnkzr@gmail.com>
2019-12-11 18:40:35 -0500

    when user changes their profile picture, the profile picture changes on all their past screams as well

--- functions/index.js
+++ functions/index.js
@@ -93,10 +93,8 @@ exports.createNotificationOnLike = functions.firestore.document('likes/{id}')
     .onDelete((snapshot) => {
         return db.doc(`/notifications/${snapshot.id}`)
         .delete()
-        .catch(err => {
-            console.error(err);
-            return;
-        })
+        .catch((err) => 
+            console.error(err));
     })
 
 
@@ -125,4 +123,57 @@ exports.createNotificationOnLike = functions.firestore.document('likes/{id}')
             return;
         });
     })
+
+
+    //
+    exports.onUserImageChange = functions.firestore.document('/users/{userId}')
+        .onUpdate((change) => {
+            console.log(change.before.data());
+            console.log(change.after.data());
+            if(change.before.data().imageUrl !== change.after.data().imageUrl){
+                console.log('image has changed')
+                const batch = db.batch();
+            return db
+                .collection('screams')
+                .where('userHandle','==', change.before.data().handle)
+                .get()
+                .then(data => {
+                    data.forEach(doc =>{
+                        const scream = db.doc(`/screams/${doc.id}`);
+                        batch.update(scream, {userImage: change.after.data().imageUrl});
+                    })
+                    return batch.commit();
+                });
+            } else return true;
+        });
+
+        exports.onScreamDelete = functions
+            .firestore.document('/users/{screamId}')
+            .onDelete((snapshot, context)=>{
+                const screamId = context.params.screamId;
+                const batch = db.batch();
+                return db.collection('comments').where('screamId', '==', screamId).get()
+                .then(data =>{
+                    data.forEach(doc => {
+                        batch.delete(db.doc(`/comments/${doc.id}`));
+                    })
+                    return db.collection('likes').where('screamId','==',screamId);
+                })
+                .then(data =>{
+                    data.forEach(doc => {
+                        batch.delete(db.doc(`/likes/${doc.id}`));
+                    })
+                    return db.collection('notifications').where('screamId','==',screamId);
+                })
+                .then(data =>{
+                    data.forEach(doc => {
+                        batch.delete(db.doc(`/notifications/${doc.id}`));
+                    })
+                    return batch.commit();
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+            })
+
 
